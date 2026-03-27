04822bfe43db9bb2dfa71330cf824cde3db93933
VINAY74-STAR <reddyvinaykumarreddy206@gmail.com>
2026-03-27 15:17:44 +0530

    UC3:Track Unique Bogie IDs (Set – HashSet)

--- src/TRAINCONSISTMANAGEMENTAPP.java
+++ src/TRAINCONSISTMANAGEMENTAPP.java
@@ -1,16 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-public class TRAINCONSISTMANAGEMENTAPP{
-
+public class TRAINCONSISTMANAGEMENTAPP {
     public static void main(String[] args) {
+        Set<String> bogieIDs = new HashSet<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== Train Consist Management System ===");
+        System.out.print("Enter number of bogies to add: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter Bogie ID " + i + ": ");
+            String id = sc.nextLine();
 
-        System.out.println("=== Train Consist Management App ===");
-
-        List<String> bogies = new ArrayList<>();
-
-        System.out.println("Train initialized successfully.");
-        System.out.println("Initial number of bogies: " + bogies.size());
-        System.out.println("test");
+            if (bogieIDs.add(id)) {
+                System.out.println("Bogie ID added successfully.");
+            } else {
+                System.out.println("Duplicate ID! Ignored.");
+            }
+        }
+        System.out.println("\n=== Unique Bogie IDs in Train ===");
+        for (String id : bogieIDs) {
+            System.out.println(id);
+        }
+        sc.close();
     }
 }
